c6229287e7d3d6b3de0a801422873086152f3d03
Mark H <mark-holding@hotmail.co.uk>
2024-10-27 13:58:29 +0000

    add createTodo module to create new todo objects

---
+++ src/createTodo.js
@@ -0,0 +1,30 @@
+import { format } from 'date-fns';
+
+const createTodo = (title, dueDate, isUrgent, isImportant, notes, projectName) => {
+    // Using the date-fns library to format the due date
+    const formattedDate = format(new Date(dueDate), 'yyyy-MM-dd');
+
+    // Factory function for creating a new todo item
+    return {
+        title,
+        dueDate: formattedDate,
+        isUrgent,
+        isImportant,
+        notes,
+        projectName,
+        isComplete: false, // Initially set to false for incomplete
+        toggleComplete() {
+            this.isComplete = !this.isComplete;
+        },
+        updateDetails({ title, dueDate, isUrgent, isImportant, notes, projectName }) {
+            this.title = title || this.title;
+            this.dueDate = dueDate ? format(new Date(dueDate), 'yyyy-MM-dd') : this.dueDate;
+            this.isUrgent = isUrgent !== undefined ? isUrgent : this.isUrgent;
+            this.isImportant = isImportant !== undefined ? isImportant : this.isImportant;
+            this.notes = notes || this.notes;
+            this.projectName = projectName || this.projectName;
+        }
+    };
+};
+
+export default createTodo;
